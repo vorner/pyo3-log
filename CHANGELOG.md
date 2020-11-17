5112957bb78f9ba20f2d2b2a9acbd73243e2779a
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-11-17 21:06:02 +0100

    Version 0.2.1
    Using arc-swap 1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+# 0.2.1
+
+* Internal dependency update (arc-swap on 1.0).
+
+# 0.2.0
+
+* Bump version of pyo3 to 0.12.
+
 # 0.1.2
 
 * Remove confusing/irrelevant copy-pasted part of README.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 #![doc(
-    html_root_url = "https://docs.rs/pyo3-log/0.2.0/pyo3-log/",
+    html_root_url = "https://docs.rs/pyo3-log/0.2.1/pyo3-log/",
     test(attr(deny(warnings)))
 )]
 #![warn(missing_docs)]
