b3312f61072ec5534e336b5d543e91e5ca09ff9d
Ben Ruijl <benruyl@gmail.com>
2025-09-18 10:02:35 +0200

    Add support for kv feature
    - Write structured data in `extra` argument
    - Set new minimal version for log crate

--- examples/hello_world/hw.py
+++ examples/hello_world/hw.py
@@ -3,8 +3,24 @@
 import logging
 import hello_world
 
-FORMAT = '%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s'
-logging.basicConfig(format=FORMAT)
-logging.getLogger().setLevel(logging.INFO)
-logging.info("Test 1")
+
+class ExtraInfoFormatter(logging.Formatter):
+    def format(self, record):
+        record_dict = record.__dict__.copy()
+        standard_keys = logging.makeLogRecord({}).__dict__.keys()
+        extras = {k: v for k, v in record_dict.items()
+                  if k not in standard_keys}
+        extras_string = f" {extras}" if extras else ""
+        formatted_message = super().format(record)
+        return f"{formatted_message}{extras_string}"
+
+
+logger = logging.getLogger('hello_world')
+handler = logging.StreamHandler()
+handler.setFormatter(ExtraInfoFormatter("%(levelname)s:%(message)s"))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+logger.info("Hello, world!", extra={"foo": 42, "bar": "baz"})
+
 hello_world.log_hello()
